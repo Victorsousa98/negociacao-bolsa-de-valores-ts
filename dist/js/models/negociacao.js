638a7e0e32d2549d97770dc37238de181f53a37c
Victorsousa98 <victorsousa708@gmail.com>
2022-04-20 22:24:30 -0300

    adiciona getters

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -10,4 +10,21 @@ export class Negociacao {
 
         //Object.freeze(this);
     }
+
+    get data(){
+        return this.#data;
+    }
+
+    get quantidade(){
+        return this.#quantidade;
+    }
+
+    get valor(){
+        return this.#valor;
+    }
+
+    get volume(){
+        return this.#quantidade * this.#valor;
+    }
+
 }

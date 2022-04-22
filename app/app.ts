2e43ce8ac96cb6e044f27515a2777abc3752ee69
Victorsousa98 <victorsousa708@gmail.com>
2022-04-22 14:57:49 -0300

    adiciona Table e view

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { negociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new negociacaoController();
 const form = document.querySelector('.form');
@@ -7,3 +8,8 @@ form.addEventListener('submit',  event => {
     event.preventDefault();
     controller.adiciona();
 })
+
+const negociacaoView = new NegociacoesView('#negociacoesView');
+const template = negociacaoView.template();
+
+console.log(template);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,11 @@
 import { negociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 const controller = new negociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
+const negociacaoView = new NegociacoesView('#negociacoesView');
+const template = negociacaoView.template();
+console.log(template);
